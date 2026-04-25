f35477fd540666d048fc0186b010b0216fda306d
Lavanya Badyal <lavanyabadyal@gmail.com>
2026-04-25 15:05:54 +0200

    update student route

--- models/student.js
+++ models/student.js
@@ -2,9 +2,29 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 
 // Define the Student Schema
+
 const studentSchema = new mongoose.Schema({
-    
-});
+    name:{
+        type: String,
+        required: true,
+        trim: true
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true
+    },
+    password:{
+        type: String,
+        required: true,
+        minlength: 6
+    },
+    courses: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Course"
+    }]
+},);                                          
 
 // Create the Student model
 const Student = mongoose.model("Student", studentSchema);
